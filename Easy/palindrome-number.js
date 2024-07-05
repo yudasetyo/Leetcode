468de6ea7fa1cd83457784380b1f4d3af8fd3489
Yuda Setyo Hidayat <yudasetyo07@gmail.com>
2024-07-05 13:43:22 +0700

    revision

--- Easy/palindrome-number.js
+++ Easy/palindrome-number.js
@@ -1,8 +1,16 @@
 const isPalindrome = (x) => {
   if (x < 0) return false;
-  let num = x.toString();
-  let reverse = num.split(" ").reverse().join("");
-  return num === reverse;
+
+  let original = x;
+  let reversed = 0;
+
+  while (x > 0) {
+    let digit = x % 10;
+    reversed = reversed * 10 + digit;
+    x = Math.floor(x / 10);
+  }
+
+  return original === reversed;
 };
 
-console.log(isPalindrome(112));
+console.log(isPalindrome(121));
